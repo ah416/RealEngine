9ddff3d6bb698db29eb5d4a95d3a9432afdaf551
ah416 <adamhenry416@gmail.com>
2023-02-05 13:19:34 -0800

    update readme.md

--- TestApp/src/App.cpp
+++ TestApp/src/App.cpp
@@ -20,7 +20,7 @@ public:
 
 		m_Framebuffer.reset(Framebuffer::Create(Application::Get().GetWindow()->GetWidth(), Application::Get().GetWindow()->GetHeight()));
 
-		m_TestMesh.reset(new Mesh("../RealEngine/objects/AmongSus.obj"));
+		m_TestMesh.reset(new Mesh("../RealEngine/objects/Puro V2.fbx"));
 		m_TestMesh->Material->DiffuseTex.reset(Texture2D::Create("../RealEngine/textures/AmongUsAlbedo.png"));
 		m_TestMesh->Material->NormalTex.reset(Texture2D::Create("../RealEngine/textures/AmongUsNormal.png"));
 
